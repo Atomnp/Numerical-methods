2c381e2c4d4cfeefcf5199af7e88a925c52e1386
aayush <neupane0403@gmail.com>
2021-02-19 09:06:36 +0545

    rk2 refactor complete

--- ODEs/RK-2.cpp
+++ ODEs/RK-2.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
+#include <cmath>
 
 //represents dy/dx=RETURN_VALUE
+//you chan change the function by changing this return value
 double df(double x, double y){
-    return (y+x);
+    return log10(x+y);
 }
 
 int main(){
@@ -19,16 +21,16 @@ int main(){
     double tolerance=0.001;
     std::cout<<"when x="<<x_init<<" ,y = "<<y_init<<std::endl;
     for(int i=0;i<n;i++){
-        double yn=y+h*df(x_init+i*h,y);
-        
-        double y_new=yn;
+        //this is exactly euler's formula
+        double y_new=y+h*df(x_init+i*h,y);
         double y_old=y;
+        std::cout<<"when x="<<x_init+(i+1)*h<<"y = "<<y_new<<std::endl;
+        //modify the obtained value of y untill we get required accuracy
         while(y_new-y_old>tolerance ){
             double mean_slope=(df(x_init+i*h,y)+df(x_init+(i+1)*h,y_new))/2;
             y_old=y_new;
             y_new=y+h*mean_slope;
             std::cout<<"when x="<<x_init+(i+1)*h<<"y = "<<y_new<<std::endl;
-            // count++;
         }
         y=y_new;
         std::cout<<"--------------------"<<std::endl;
